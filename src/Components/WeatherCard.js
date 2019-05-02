e4fba272b07ac0fa3ef805e64a09bbe95bd579bf
chloe xu <chloeqxu44@gmail.com>
2019-05-02 10:32:17 -0400

    create WeatherCard Component

---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class WeatherCard extends React.Component {
+    render() {
+        return (
+        <div >
+            <h1>Hello</h1>
+        </div>
+    );
+    }
+    
+}
+
+export default WeatherCard;
