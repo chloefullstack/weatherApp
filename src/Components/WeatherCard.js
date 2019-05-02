f68c9d91a6ef651f99e18360b0b31e7683b5ce50
chloe xu <chloeqxu44@gmail.com>
2019-05-02 12:32:47 -0400

    add one weather

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
-
+import './WeatherCard.css';
+import sunny from '../images/sunny.png';
 class WeatherCard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            day: 'Wed',
+            weather: 'sunny',
+            highest: 78,
+            lowest:67
+        }
+    }
     render() {
-        return (
-        <div >
-            <h1>Hello</h1>
+        // let { data } = this.state
+        return <div>
+            <div>
+                {/* {data.map((item, index) => ( */}
+                    <div className='card' >
+                        <div>{this.state.day}</div>
+                        <img src={sunny}></img>
+                        <div>{this.state.highest}  {this.state.lowest}</div>
+                    </div>
+                {/* )) */}
+                } 
+            </div>
         </div>
-    );
+            
+        
+        
     }
     
 }
