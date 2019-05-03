6956432cd6de328873e18d1952c1465847392792
chloe xu <chloeqxu44@gmail.com>
2019-05-02 21:56:10 -0400

    fetch and display data from local weather.js

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -1,45 +1,66 @@
 import React from 'react';
 import './WeatherCard.css';
 import sunny from '../images/sunny.png';
-import axios from "axios";
-import mockData from "../mockData/weather"
+import cloudy from '../images/cloudy.png';
+import lightrain from '../images/lightrain.png';
+import lightsun from '../images/lightsun.png';
+import rainy from '../images/rainy.png';
+import snowy from '../images/snowy.png';
+import windy from '../images/windy.png';
+
+import mockData from "../mockData/weather.js";
+import moment from 'moment';
+
 
 class WeatherCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            city: '',
-            day: 'Wed',
-            weather: 'sunny',
-            highest: 78,
-            lowest:67
+            weatherData: []
         }
     }
 
     componentDidMount() {
         this.getWeatherData()
     }
-
+    // get all data from local file
     getWeatherData = () => {
-        mockData.then(res => {
+        mockData().then(res => {
             this.setState({
-                city: res.city.name,
-                day: res.list.dt_txt,
-                weather: res.list.weather.main,
-                highest: res.list.main.temp_max,
-                lowest: res.list.main.temp_min
+                weatherData: res
             })
+            console.log(res)
         })
+        
+    }
+    //get images depend on description fetch from res
+    getImage = (main) => {
+        if (main == 'sunny') { return sunny}
+        else if (main == 'rainy') { return rainy}
+        else if (main == 'windy') { return windy}
+        else if (main == 'lightsun') { return lightsun}
+        else if (main == 'lightrain') { return lightrain}
+        else if (main == 'snowy') { return snowy}
+        else if (main == 'cloudy') { return cloudy}
+
+
+    }
+    // change date to day by using moment
+    changeDate = (dt_txt) => {
+        return moment(dt_txt).format('ddd')
     }
     render() {
-        // let { data } = this.state
+        let { weatherData } = this.state
         return <div>
-            <div>
-                <div className='card' >
-                    <div>{this.state.day}</div>
-                    <img src={sunny}></img>
-                    <div>{this.state.highest}  {this.state.lowest}</div>
+            <div className='setting'>
+                {weatherData.map((item, index) => (
+                <div className='card' key={index}>
+                    <div>{item.cityname}</div>
+                    <div>{this.changeDate(item.dt_txt)}</div>
+                    <img src={this.getImage(item.weather.main)}></img>
+                    <div>{item.main.temp_max}  {item.main.temp_min}</div>
                 </div>
+                ))}
             </div>
         </div>
     }
--- src/mockData/weather.js
+++ src/mockData/weather.js
@@ -1,46 +1,45 @@
 const mockData = [
-    {
-        "coord": {
-            "lon": -0.13,
-            "lat": 51.51
-        },
-        "weather": [
-            {
-                "id": 300,
-                "main": "Drizzle",
-                "description": "light intensity drizzle",
-                "icon": "09d"
-            }
-        ],
-        "base": "stations",
-        "main": {
-            "temp": 280.32,
-            "pressure": 1012,
-            "humidity": 81,
-            "temp_min": 279.15,
-            "temp_max": 281.15
-        },
-        "visibility": 10000,
-        "wind": {
-            "speed": 4.1,
-            "deg": 80
-        },
-        "clouds": {
-            "all": 90
-        },
-        "dt": 1485789600,
-        "sys": {
-            "type": 1,
-            "id": 5091,
-            "message": 0.0103,
-            "country": "GB",
-            "sunrise": 1485762037,
-            "sunset": 1485794875
-        },
-        "id": 2643743,
-        "name": "London",
-        "cod": 200
-    }
+    {"coord": {"lon": -0.13,"lat": 51.51},
+    "weather": {"id": 300,"main": "lightsun","description": "light intensity drizzle","icon": "09d"},
+    "base": "stations",
+    "main": {"temp": 280.32,"pressure": 1012,"humidity": 81,"temp_min": 67,"temp_max": 78},
+    "visibility": 10000,
+    "wind": {"speed": 4.1,"deg": 80},
+    "clouds": {"all": 90},
+    "dt": 1485789600,
+    "sys": {"type": 1,"id": 5091,"message": 0.0103,"country": "GB","sunrise": 1485762037,"sunset": 1485794875},
+    "dt_txt":"2014-07-23 09:00:00",
+    "id": 2643743,
+    "cityname": "Newark",
+    "cod": 200
+    },
+    {"coord": {"lon": -0.13,"lat": 51.51},
+    "weather": {"id": 300,"main": "windy","description": "light intensity drizzle","icon": "09d"},
+    "base": "stations",
+    "main": {"temp": 280.32,"pressure": 1012,"humidity": 81,"temp_min": 66,"temp_max": 83},
+    "visibility": 10000,
+    "wind": {"speed": 4.1,"deg": 80},
+    "clouds": {"all": 90},
+    "dt": 1485789600,
+    "sys": {"type": 1,"id": 5091,"message": 0.0103,"country": "GB","sunrise": 1485762037,"sunset": 1485794875},
+    "dt_txt":"2014-07-24 09:00:00",
+    "id": 2643743,
+    "cityname": "Newark",
+    "cod": 200
+    },{"coord": {"lon": -0.13,"lat": 51.51},
+    "weather": {"id": 300,"main": "rainy","description": "light intensity drizzle","icon": "09d"},
+    "base": "stations",
+    "main": {"temp": 280.32,"pressure": 1012,"humidity": 81,"temp_min": 65,"temp_max": 77},
+    "visibility": 10000,
+    "wind": {"speed": 4.1,"deg": 80},
+    "clouds": {"all": 90},
+    "dt": 1485789600,
+    "sys": {"type": 1,"id": 5091,"message": 0.0103,"country": "GB","sunrise": 1485762037,"sunset": 1485794875},
+    "dt_txt":"2014-07-25 09:00:00",
+    "id": 2643743,
+    "cityname": "Newark",
+    "cod": 200
+    },
 ]
 const mockPromise = () => {
     return new Promise((resolve) => {
